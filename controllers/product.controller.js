9de6954c908c0bcd803051e99ac991a683d3d2ed
rlawndks4 <rlawndks0423@naver.com>
2023-09-20 23:14:43 +0900

    FeaT: update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@
 import { pool } from "../config/db.js";
 import { checkIsManagerUrl } from "../utils.js/function.js";
 import { deleteQuery, getSelectQuery, insertQuery, selectQuerySimple, updateQuery } from "../utils.js/query-util.js";
-import { checkDns, checkLevel, isItemBrandIdSameDnsId, lowLevelException, response, settingFiles } from "../utils.js/util.js";
+import { checkDns, checkLevel, isItemBrandIdSameDnsId, lowLevelException, makeObjByList, response, settingFiles } from "../utils.js/util.js";
 import 'dotenv/config';
 
 const table_name = 'products';
@@ -23,9 +23,16 @@ const productCtrl = {
             sql += ` LEFT JOIN product_categories ON ${table_name}.category_id=product_categories.id `;
             sql += ` WHERE ${table_name}.brand_id=${decode_dns?.id} `;
             if (category_id) sql += ` AND ${table_name}.category_id=${category_id} `;
-            console.log(req.query)
             let data = await getSelectQuery(sql, columns, req.query);
-
+            let item_id_list = [0];
+            item_id_list = [...item_id_list, ...data.content.map(item => { return item.id })];
+            let budget_data = await pool.query(`SELECT * FROM budget_products WHERE product_id IN (${item_id_list.join()}) AND user_id=${decode_user?.id ?? 0}`)
+            budget_data = budget_data?.result;
+            budget_data = makeObjByList('product_id', budget_data);
+            for (var i = 0; i < data?.content.length; i++) {
+                let budget_item = budget_data[`${data?.content[i]?.id}`] ?? []
+                data.content[i]['budget'] = budget_item[0] ?? {}
+            }
             return response(req, res, 100, "success", data);
         } catch (err) {
             console.log(err)
@@ -43,7 +50,7 @@ const productCtrl = {
             let data = await pool.query(`SELECT * FROM ${table_name} WHERE id=${id}`)
             data = data?.result[0];
             data['product_sub_imgs'] = JSON.parse(data?.product_sub_imgs ?? "[]");
-            let budget_product = await pool.query(`SELECT * FROM budget_products WHERE user_id=${decode_dns?.id??0} AND product_id=${id}`);
+            let budget_product = await pool.query(`SELECT * FROM budget_products WHERE user_id=${decode_user?.id ?? 0} AND product_id=${id}`);
             budget_product = budget_product?.result[0];
             data['budget'] = budget_product;
             if (!isItemBrandIdSameDnsId(decode_dns, data)) {
@@ -83,7 +90,7 @@ const productCtrl = {
             obj = { ...obj, ...files };
 
             let result = await insertQuery(`${table_name}`, obj);
-
+            console.log(result)
             return response(req, res, 100, "success", {})
         } catch (err) {
             console.log(err)
@@ -149,9 +156,22 @@ const productCtrl = {
             const decode_dns = checkDns(req.cookies.dns);
             const {
                 product_id,
-                budget_price
+                budget_price,
+                user_id
             } = req.body;
-
+            let data = await pool.query(`SELECT * FROM budget_products WHERE user_id=${user_id} AND product_id=${product_id} `)
+            data = data?.result[0];
+            if (data) {
+                let result = await updateQuery(`budget_products`, {
+                    budget_price
+                }, data?.id);
+            } else {
+                let result = await insertQuery(`budget_products`, {
+                    budget_price,
+                    product_id,
+                    user_id
+                }, data?.id);
+            }
             return response(req, res, 100, "success", {})
         } catch (err) {
             console.log(err)
